57beebb96cf2c07987cf6a8cddab3af9784d9ab8
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-02-24 14:35:17 +0100

    logo install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import img from './/pics/omworks_logo.svg';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>OMWORKS - YOGA CREATIVES</h1>
+      <img src={img} alt="omworks - yoga creatives"/>
+          <h1>YOGACREATIVES</h1>
       </header>
     </div>
   );
