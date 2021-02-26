d41a4d8a46a0897b8d9e23ceb3c7635aea2aa170
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-02-26 13:00:42 +0100

    css changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import img from './/pics/omworks_b.svg';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={img} alt="omworks - yoga creatives"/>
-      </header>
-    </div>
+    <Container fluid>
+  <Row>
+    <header>
+      <Col><img src={img} alt="omworks - yoga creatives"/></Col>
+    </header>
+  </Row>
+</Container>
+    // <div className="App">
+    //   <header className="App-header">
+    //   <img src={img} alt="omworks - yoga creatives"/>
+    //   </header>
+    // </div>
   );
 }
 
