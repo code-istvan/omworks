4a69492df9ca50062fe33e172c831708e1e55ee6
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-02-26 15:35:21 +0100

    img position set

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import img from './/pics/omworks_b.svg';
-
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import img from ".//pics/omworks_final.svg";
 
 function App() {
   return (
-    <Container fluid>
-  <Row>
-    <header>
-      <Col><img src={img} alt="omworks - yoga creatives"/></Col>
-    </header>
-  </Row>
-</Container>
+    <Container>
+      <Row>
+        <Col>
+          <img src={img} alt="omworks - yoga creatives" />
+        </Col>
+      </Row>
+    </Container>
     // <div className="App">
     //   <header className="App-header">
     //   <img src={img} alt="omworks - yoga creatives"/>
